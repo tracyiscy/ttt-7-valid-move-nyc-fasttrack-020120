49ce3a1a9ff838397993e6bb52b75586e8057bdd
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-01 20:39:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board,index)
+def valid_move?(board,index)
   if board[index] == " "
     return true
   end
